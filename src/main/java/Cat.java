3b0567d6ee82abd4058b2e4ada09aca2399daa5c
Gev <129871410+Mazmanyak@users.noreply.github.com>
2023-12-02 18:04:41 +0400

    added class Cat

---
+++ src/main/java/Cat.java
@@ -0,0 +1,64 @@
+import java.util.Objects;
+
+
+public class Cat {
+
+    private int id;
+    private String name;
+    private int age;
+
+    public Cat() {
+    }
+
+    public Cat(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return id == cat.id && age == cat.age && Objects.equals(name, cat.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
